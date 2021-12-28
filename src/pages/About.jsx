4347ce64f1354bcb57cb3f0a87c41620c11d1d7c
ghawthorne66 <ghawthorne@me.com>
2021-12-28 15:40:53 +0100

    change about details

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,10 +5,8 @@ function About() {
         <p className='mb-4 text-2xl font-light'>
           A React app to search GitHub profiles and see profile details. This
           project is done with
-          <a href='https://frontsightmarketing.com'>
-            {' '}
+         
             ReactJS and Github API
-          </a>{' '}
          
           <strong>
             <a href='https://github-finder-gvlfsnu4q-ghawthorne.vercel.app/'></a>
@@ -18,12 +16,7 @@ function About() {
         <p className='text-lg text-gray-400'>
           Version <span className='text-white'>1.0.0</span>
         </p>
-        <p className='text-lg text-gray-400'>
-         Twitter
-          <a className='text-white' href='https://twitter.com/gshawthorne'>
-            
-          </a>
-        </p>
+       
       </>
     )
   }
