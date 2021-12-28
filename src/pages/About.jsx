856728390dab5ed05ede99328c0292312f812ade
ghawthorne66 <ghawthorne@me.com>
2021-12-28 15:35:14 +0100

    change about details

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,14 +4,14 @@ function About() {
         <h1 className='text-6xl mb-4'>Github Finder</h1>
         <p className='mb-4 text-2xl font-light'>
           A React app to search GitHub profiles and see profile details. This
-          project is part of the
-          <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
+          project is done with
+          <a href='https://frontsightmarketing.com'>
             {' '}
-            React Front To Back
+            ReactJS and Github API
           </a>{' '}
-          Udemy course by
+         
           <strong>
-            <a href='https://traversymedia.com'> Brad Traversy</a>
+            <a href='https://github-finder-gvlfsnu4q-ghawthorne.vercel.app/'></a>
           </strong>
           .
         </p>
@@ -19,9 +19,9 @@ function About() {
           Version <span className='text-white'>1.0.0</span>
         </p>
         <p className='text-lg text-gray-400'>
-          Layout By:
-          <a className='text-white' href='https://twitter.com/hassibmoddasser'>
-            Hassib Moddasser
+         Twitter
+          <a className='text-white' href='https://twitter.com/gshawthorne'>
+            
           </a>
         </p>
       </>
